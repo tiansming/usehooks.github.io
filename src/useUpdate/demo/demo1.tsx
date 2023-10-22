d65b64697775dc3373a342ae656db88933ec26c9
tianshuangming123 <tianshuangming@swifthealth.cn>
2023-10-22 15:36:40 +0800

    chore: run tests on 构建

--- src/useUpdate/demo/demo1.tsx
+++ src/useUpdate/demo/demo1.tsx
@@ -1,10 +1,18 @@
+/*
+ * @Author: tianshuangming123 tianshuangming@swifthealth.cn
+ * @Date: 2023-10-22 14:50:57
+ * @LastEditors: tianshuangming123 tianshuangming@swifthealth.cn
+ * @LastEditTime: 2023-10-22 15:35:49
+ * @FilePath: \useHooks\src\useUpdate\demo\demo1.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /**
  * title: 基础用法
  * desc: 强制组件重新渲染。
  */
 
-import React from 'react';
 import { useUpdate } from 'encode-hooks';
+import React from 'react';
 
 export default () => {
   const update = useUpdate();
@@ -13,7 +21,7 @@ export default () => {
     <>
       <div>Time: {Date.now()}</div>
       <button type="button" onClick={update} style={{ marginTop: 8 }}>
-        update
+        update重新构建测试
       </button>
     </>
   );
